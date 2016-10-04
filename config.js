c51a6db3b0d4250cbe254ff90e27ed1be5724a0c
Antonio Pardo <apardo.socket@gmail.com>
2016-10-04 17:30:18 +0200

    fix tipo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     /*'secret': process.env.SECRET,
     'database': process.env.MONGO_DSN,*/
-    'secret': prompt.env.SECRET,
+    'secret': process.env.SECRET,
     'database': process.env.MONGO_DSN,
     "port" : process.env.PORT || 3000
 
