9fc8e4d618db5584407d81945ce7c4a92b659542
idoctnef <idoctnef@openmailbox.org>
2016-10-04 17:54:20 +0200

    www added

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ router.get('/', function(req, res) {
   res.send("Hello world!");
 });
 app.use(router);*/
-app.use(express.static(__dirname + '/web'));
+app.use(express.static(__dirname + '/www'));
 
 
 //CORS
